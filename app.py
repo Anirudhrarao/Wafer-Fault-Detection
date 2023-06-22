cd12ae088410b433d545d9097389613390baa6c0
Anirudhrarao <raorudhra16@gmail.com>
2023-06-22 16:34:52 +0530

    Adde one more module for model training logging and added some animation to frontend

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
 import sys
+import time
 from flask import Flask, render_template, jsonify, request, send_file
 from src.exception import CustomException
-from src.logger import logging
+from src.logger import logging, log_path, LOG_FILE
 
 from src.pipelines.train_pipeline import TrainingPipeline, DataTransformation
 from src.pipelines.predict_pipeline import PredictionPipeline
@@ -34,6 +35,22 @@ def upload():
                             as_attachment=True)
         else:
             return render_template('upload_file.html')
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+@app.route('/logged')
+def training_logger():
+    try:
+        
+        LOG_FILE_PATH = os.path.join(log_path,LOG_FILE)
+        log_contents = []
+
+        with open(LOG_FILE_PATH, 'r') as file:
+            logs = file.readlines()
+            filtered_logs = [log for log in logs if 'root' in log]
+            log_contents.extend(filtered_logs)
+
+        return render_template('logs.html', logs=log_contents)
     except Exception as e:
         raise CustomException(e,sys)
 
